07382fc252cc0947bf395b8be3c61f562010a49c
lewdiedoeswubswubs <lanceedansantos@gmail.com>
2025-10-22 17:12:58 +0800

    New changes

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -21,12 +21,6 @@ class SupplierSeeder extends Seeder
             [
                 'name' => 'ABC Agricultural Supplies',
                 'company_name' => 'ABC Agricultural Supplies Co.',
-                'contact_persons' => json_encode([
-                    ['name' => 'John Smith', 'email' => 'john@abcagri.com', 'phone' => '+1 (555) 123-4567'],
-                    ['name' => 'Maria Garcia', 'email' => 'maria@abcagri.com', 'phone' => '+1 (555) 123-4568']
-                ]),
-                'phone_numbers' => json_encode(['+1 (555) 123-4567', '+1 (800) 123-4567']),
-                'email_addresses' => json_encode(['info@abcagrisupplies.com', 'sales@abcagrisupplies.com']),
                 'address' => '123 Farm Supply Rd, Agricultural City',
                 'website' => 'https://abcagrisupplies.com',
                 'tax_id' => 'TIN-123456789',
@@ -37,11 +31,6 @@ class SupplierSeeder extends Seeder
             [
                 'name' => 'Green Thumb Tools',
                 'company_name' => 'Green Thumb Tools Inc.',
-                'contact_persons' => json_encode([
-                    ['name' => 'Robert Johnson', 'email' => 'robert@greenthumb.com', 'phone' => '+1 (555) 987-6543']
-                ]),
-                'phone_numbers' => json_encode(['+1 (555) 987-6543', '+1 (800) 987-6543']),
-                'email_addresses' => json_encode(['sales@greenthumbtools.com', 'support@greenthumbtools.com']),
                 'address' => '456 Tool Lane, Industrial Park',
                 'website' => 'https://greenthumbtools.com',
                 'tax_id' => 'TIN-987654321',
@@ -52,12 +41,6 @@ class SupplierSeeder extends Seeder
             [
                 'name' => 'Organic Growth Solutions',
                 'company_name' => 'Organic Growth Solutions LLC',
-                'contact_persons' => json_encode([
-                    ['name' => 'Sarah Wilson', 'email' => 'sarah@organicgrowth.com', 'phone' => '+1 (555) 555-1234'],
-                    ['name' => 'David Kim', 'email' => 'david@organicgrowth.com', 'phone' => '+1 (555) 555-1235']
-                ]),
-                'phone_numbers' => json_encode(['+1 (555) 555-1234', '+1 (800) 555-1234']),
-                'email_addresses' => json_encode(['info@organicgrowth.com', 'support@organicgrowth.com']),
                 'address' => '789 Organic Way, Eco Park',
                 'website' => 'https://organicgrowth.com',
                 'tax_id' => 'TIN-456789123',
