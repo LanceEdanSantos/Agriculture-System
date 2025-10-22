bb15066854126819e93bfa0bba861efebfa52505
lewdiedoeswubswubs <lanceedansantos@gmail.com>
2025-10-22 16:41:17 +0800

    New changes

--- app/Filament/Resources/InventoryItemResource/Pages/CreateInventoryItem.php
+++ app/Filament/Resources/InventoryItemResource/Pages/CreateInventoryItem.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateInventoryItem extends CreateRecord
 {
     protected static string $resource = InventoryItemResource::class;
+    protected function getRedirectUrl(): string
+    {
+        // This makes it go back to the table page after creating a record
+        return $this->getResource()::getUrl('index');
+    }
 }
