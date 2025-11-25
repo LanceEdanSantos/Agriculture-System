cdb19b6ae418adce4f785741f827814b976dd8e2
lewdiedoeswubswubs <lanceedansantos@gmail.com>
2025-11-26 06:08:38 +0800

    new changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,6 +39,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->databaseNotificationsPolling('5s')
             ->pages([
                 Dashboard::class,
             ])
